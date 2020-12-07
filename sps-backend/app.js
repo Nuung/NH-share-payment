91b3f414989bd634ee5c3e6b51c257ad10e7caba
Nuung <qlgks1@naver.com>
2020-12-07 11:49:19 +0900

    temp

--- sps-backend/app.js
+++ sps-backend/app.js
@@ -38,7 +38,7 @@ app.set('jwt-secret', appConfig.parsed.JWT_SECRET); // set the secret key variab
 userApi(app);
 userCardApi(app);
 userPaymentApi(app);
-snsBoardApi(app);
+// snsBoardApi(app);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
