1be37b15e923b55eba9ed21cd242daf7e867f8f3
enriver <riverkeh@naver.com>
2020-12-13 04:38:44 +0900

    Update Flask code

--- sps-machine-learning/server/app.py
+++ sps-machine-learning/server/app.py
@@ -1,45 +1,20 @@
-from flask import Flask, render_template,request
-import requests
-import json
-import pandas as pd
-#from random import randint
-#import os
-
+from flask import Flask,request,jsonify
+import os
 
 app=Flask(__name__)
 
+@app.route('/getUser',methods=['POST'])
+def getUser():
+    user=request.get_json()
+    user_id=user['userID']
 
-@app.route('/go-server',methods=('GET','POST'))
+    path=os.getcwd().rstrip('server')
 
-def index():
-    if request.method=='POST':
-        
-        IsTuno=request.form.get('IsTuno')
-       
-        headers = {
-            'accept': 'application/json',
-            'Content-Type': 'application/json',
-        }
+    os.system('python {path}user_cluster.py {user_id}'.format(path=path, user_id=user_id))
 
-        data = '{ "Header": { "ApiNm": "InquireCreditCardAuthorizationHistory", "Tsymd": "20201213", "Trtm": "112428", "Iscd": "000571", "FintechApsno": "001", "ApiSvcCd": "CardInfo", "IsTuno": "'+IsTuno+'", "AccessToken": "38020bbc17d9f5dc06f82f69762b441f2b259cbfbf4b4b18a067d81c9515c8aa" }, "FinCard": "00829101234560000112345678919", "IousDsnc": "1", "Insymd": "20191105", "Ineymd": "20191109", "PageNo": "1", "Dmcnt": "15"}'
-
-        response = requests.post('https://developers.nonghyup.com/InquireCreditCardAuthorizationHistory.nh', headers=headers, data=data)
-        js=response.json()
-
-        df=pd.DataFrame(js['REC'])
-        print(df.head())
-
-        #path=os.getcwd().rstrip('server')
-        #user='abc412@nh.com'
-        #os.system('python {path}user_cluster.py {user}'.format(path=path,user=user))
-
-        return render_template('send_istuno.html',IsTuno=IsTuno)
-
-    elif request.method=='GET':
-        IsTuno=' '
-        return render_template('send_istuno.html',IsTuno=IsTuno)
+    return 'OK'
 
 if __name__=="__main__":
-    app.run(debug=True)
-    #app.run(host='44.242.175.149', port='3001',debug=True)
+    #app.run(debug=True)
+    app.run(host='44.242.175.149', port='3001',debug=True)
 
