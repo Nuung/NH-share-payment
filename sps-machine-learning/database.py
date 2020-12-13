e74c4f2099498b4dc57f3ca8b1adde4a62ac9e75
enriver <riverkeh@naver.com>
2020-12-13 16:43:23 +0900

    cluster table

--- sps-machine-learning/database.py
+++ sps-machine-learning/database.py
@@ -14,9 +14,10 @@ class database():
     # 유저 카드승인 내역 정보 받아오기
     def get_userpayment(self):
         today="'"+datetime.datetime.today().strftime('%Y-%m-%d')+"'"
-        before30days="'"+(datetime.datetime.today()-datetime.timedelta(days=50)).strftime('%Y-%m-%d')+"'"
+        before30days="'"+(datetime.datetime.today()-datetime.timedelta(days=30)).strftime('%Y-%m-%d')+"'"
 
         sql = "SELECT user_id, Trdd, Txtm, Usam, category FROM user_payments WHERE Trdd <="+today+"and Trdd >="+before30days+";"
+        #sql = "SELECT user_id, Trdd, Txtm, Usam, category FROM user_payments WHERE Trdd <="+"'2020-12-05'"+"and Trdd >="+"'2020-11-05'"+";"
         self.cursor.execute(sql)
         self.conn.commit()
 
